5db5048f00e0f54c967300de761e7f6fa848583c
Mark Delgado <markdelgado.md@hotmail.com>
2022-08-08 18:57:05 -0400

    commit tree 4fe0ba308e39800fdb6740cd17a0844e3029ab19

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
 # Your code here!
+def my_method (param) 
+    puts "Running my method"
+    param + 1
+
+end 
+
+def greet_programmer 
+puts "Hello, programmer!"
+end
+
+def greet (name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+
+end
+
+def add (num1, num2)
+return num1 + num2
+end
+
+def halve (number) 
+return nil unless number.class == Integer
+number/2
+end
